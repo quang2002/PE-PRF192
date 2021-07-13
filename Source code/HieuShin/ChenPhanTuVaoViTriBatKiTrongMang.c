3d7a287720d064ff586f362bf59722710b09cbff
HieuShin <86659863+HieuShin@users.noreply.github.com>
2021-07-13 15:28:13 +0700

    Add files via upload

--- Source code/HieuShin/ChenPhanTuVaoViTriBatKiTrongMang.c
+++ Source code/HieuShin/ChenPhanTuVaoViTriBatKiTrongMang.c
@@ -1,15 +1,19 @@
 #include <stdio.h>
+void insert(int *arr, int *n, int position, int value)
+{
+    int i;
+    for (i = *n; i > position; --i)
+        arr[i] = arr[i - 1];
+    arr[position] = value;
+    (*n)++;
+}
 int main()
 {
     int n, a[100], i, p, value;
     scanf("%d", &n);
     for(i = 0; i < n; ++i) scanf("%d", &a[i]);
     scanf("%d%d", &p, &value);
-    p--;
-    for (i = n; i > p; --i)
-        a[i] = a[i - 1];
-    a[p] = value;
-    n++;
+    insert(a, &n, p - 1, value);
     for(i = 0; i < n; ++i) printf("%d\n", a[i]);
     return 0;
 }
---
+++ Source code/HieuShin/ThayTheChuoi.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <ctype.h>
+void strreplace(char *string, const char *find, const char *replaceWith){
+    if(strstr(string, replaceWith) != NULL)
+    {
+        char *temporaryString = malloc(strlen(strstr(string, find) + strlen(find)) + 1);
+        strcpy(temporaryString, strstr(string, find) + strlen(find));    //Create a string with what's after the replaced part
+        *strstr(string, find) = '\0';    //Take away the part to replace and the part after it in the initial string
+        strcat(string, replaceWith);    //Concat the first part of the string with the part to replace with
+        strcat(string, temporaryString);    //Concat the first part of the string with the part after the replaced part
+        free(temporaryString);    //Free the memory to avoid memory leaks
+    }
+}
+int main()
+{
+    char s[100], old[100], rpl[100];
+    gets(s);
+    gets(old);
+    gets(rpl);
+    strreplace(s, old, rpl);
+    puts(s);
+    return 0;
+}
